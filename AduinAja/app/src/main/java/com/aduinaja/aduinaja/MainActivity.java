b57b906daf6a078af08f8759ecf4d5e1aa714d44
Andri Ma'ruf Anshori <and_drie69@yahoo.com>
2016-02-10 14:40:46 -0800

    update

--- AduinAja/app/src/main/java/com/aduinaja/aduinaja/MainActivity.java
+++ AduinAja/app/src/main/java/com/aduinaja/aduinaja/MainActivity.java
@@ -5,15 +5,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.LoggingBehavior;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.rey.material.app.ThemeManager;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,8 +50,52 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent next = new Intent(MainActivity.this, TabFragment.class );
-                startActivity(next);
+
+                    // App code
+
+                    // login ok get access token
+                GraphRequest request = GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                                if (BuildConfig.DEBUG) {
+                                    FacebookSdk.setIsDebugEnabled(true);
+                                    FacebookSdk
+                                            .addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
+                                    System.out
+                                            .println("AccessToken.getCurrentAccessToken()"
+                                                    + AccessToken
+                                                    .getCurrentAccessToken()
+                                                    .toString());
+                                    //Log.i("FB", )
+                                    Profile.getCurrentProfile().getId();
+                                    Profile.getCurrentProfile().getFirstName();
+                                    Profile.getCurrentProfile().getLastName();
+                                    Profile.getCurrentProfile().getProfilePictureUri(50, 50);
+                                    Intent next = new Intent(MainActivity.this, TabFragment.class );
+                                    startActivity(next);
+                                    //String email=UserManager.asMap().get(“email”).toString();
+                                }
+                            }
+                        });
+                    request.executeAsync();
+                /*  Bundle parameters = new Bundle();
+                    parameters
+                            .putString("fields",
+                                    "id,firstName,lastName,name,email,gender,birthday,address");
+                    request.setParameters(parameters);
+
+
+
+                    Intent loginintent = new Intent(getActivity(),
+                            EditProfile.class);
+                    startActivity(loginintent);
+                    System.out.println("XXXX " + getId());
+                 */
+                    //makeJsonObjReq();
+
             }
 
             @Override
